4a16f10ee8f91e272b18de58691f57b30a23347e
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-30 12:23:04 +0600

    inventory added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner';
+import Inventory from '../Inventory/Inventory';
 
 const Home = () => {
+    const [inventories, setInventories] = useState([]);
+
+    useEffect( () => {
+        fetch('http://localhost:5000/inventory')
+        .then(res => res.json())
+        .then(data => setInventories(data));
+    },[])
     return (
         <div>
             <Banner></Banner>
+            <div className='container'>
+            <h3>This is inventory:{inventories.length}</h3>
+            <div className='row gy-5'>
+            {
+                inventories.map(inventory => <Inventory key={inventory._id}
+                inventory={inventory}
+                ></Inventory>)
+            }
+            </div>
+        </div>
+            
         </div>
     );
 };
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Inventory.css';
 
-const Inventory = () => {
+const Inventory = ({ inventory }) => {
+    const { name, picture, body, quantity, price, suplier } = inventory;
+    console.log(inventory);
     return (
-        <div>
-            <h3>This is inventory</h3>
+        <div className="col-md-4">
+            <div className="card">
+                <img src={picture} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <h6>Suplier Name: {suplier}</h6>
+                    <p>Quantity: <small>{quantity}</small></p>
+                    <p>price:<small> ${price}</small></p>
+                    <p className="card-text">
+                        {body}
+                    </p>
+                    <button className='btn_1'>Manage</button>
+                </div>
+            </div>
         </div>
     );
 };
