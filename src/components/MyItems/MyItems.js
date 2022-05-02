c0efbe48d967f1f25c30f0cf9eebe40c6fd05fb8
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-05-02 17:35:58 +0600

    my items page added

--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import useInventories from '../hooks/useInventories';
+import './MyItems.css';
 
 const MyItems = () => {
+    const [inventories, setInventories] = useInventories();
+
+    const handleButton = id =>{
+        const proceed = window.confirm('Do you want to delete?');
+        if (proceed){
+            const url = `http://localhost:5000/inventory/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const remaining = inventories.filter(inventory => inventory._id !== id);
+                setInventories(remaining);
+            });
+
+        }
+    }
+
     return (
-        <div>
-            
+        <div className='container'>
+            <h2>Chosse Your Inventory</h2>
+            <div>
+            {
+                inventories.map(inventory => <div key={inventory._id}>
+                <div>
+                <h5>{inventory.name}</h5>
+                <button onClick={() => handleButton(inventory._id)}>Delete</button>
+                </div>
+                </div>)
+            }
+            </div>
         </div>
     );
 };
